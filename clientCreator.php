bde0bbd55db80d3664bcf30dcbde5b82f9cbdebe
hoommaah <hcmalijan@up.edu.ph>
2016-06-21 17:40:09 +0800

    onject oriented deisgn

---
+++ clientCreator.php
@@ -0,0 +1,23 @@
+<?php
+  use GuzzleHttp\Client;
+  use GuzzleHttp\Psr7\Request;
+
+  chdir(dirname(__DIR__));
+
+  require 'vendor/autoload.php';
+
+  class clientCreator{
+    private static $client;
+
+    public static function getInstance(){
+      if(!self::$client){
+        self::$client = new GuzzleHttp\Client([      //create new vendor client
+          'base_uri' => 'https://rebit.ph/api/v1/',
+        ]);
+        echo 'created';
+      }
+      return self::$client;
+    }
+
+  }//close class
+?>
--- vendors.php
+++ vendors.php
@@ -5,18 +5,40 @@
   chdir(dirname(__DIR__));
 
   require 'vendor/autoload.php';
-  $vendorToken = 'xZ4A1TuPxx-Vyywo1FanrvxGH59ZCs6X';
+  require 'clientCreator.php';
 
-  $vendors = new GuzzleHttp\Client([
-    'base_uri' => 'https://rebit.ph/api/v1/',
-  ]);
 
-  $response = $vendors->request('GET',"vendors/${vendorToken}");
-  $body = $response->getBody();
+  class Vendors{
+    var $vendorToken; //vendor id
+    var $vendors;     //vendor client
 
-  echo $body."\n";
-  echo $response->getStatusCode()."\n"; // 200
-  echo $response->getReasonPhrase()."\n"; // OK
-  echo $response->getProtocolVersion()."\n"; // 1.1
+    function __construct($vendorToken){
+      $this->vendorToken = $vendorToken;
+    }//close constructor
+
+    function getVendor(){               //retrieve a vendor obj given a vendor id
+      $tempClient = clientCreator::getInstance();
+      $response = $tempClient->request('GET',"vendors/$this.{vendorToken}");
+      $body = $response->getBody();
+
+      echo $body."\n";
+      echo $response->getStatusCode()."\n"; // 200
+      echo $response->getReasonPhrase()."\n"; // OK
+      echo $response->getProtocolVersion()."\n"; // 1.1
+    }//close getVendor
+  }//close class
+
+  $newVendor = new Vendors('xZ4A1TuPxx-Vyywo1FanrvxGH59ZCs6X');
+  $newVendor->getVendor();
+
+
+
+
+
+
+
+
+  //$NewVendors = new vendorToken('xZ4A1TuPxx-Vyywo1FanrvxGH59ZCs6X');
+
 
 ?>
