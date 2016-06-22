252a98e74410363674bc7166a69705c3b37c0256
cquintos <jdquintos@up.edu.ph>
2016-06-22 18:24:21 +0800

    created vendor resquests

--- users.php
+++ users.php
@@ -17,26 +17,27 @@
       $response = clientCreator::getInstance()->request('GET', "user?token=$this->userToken");
       $body = json_decode($response->getBody(), true);  //decodes the resposnce body
       $data = json_encode($body['user']); // encodes back to json with out the user key
-      return $data;
+      echo $data;
     }
 
     function updateUser ($param) {  //updates user given a valid user token
-      $response = clientCreator::getInstance()->request('PUT', "user", ['json' => $param]);
+      $response = clientCreator::getInstance()->request('PUT', "user?token=$this->userToken", ['json' => $param]);
       echo $response->getStatusCode();
     }
   }
 
-  $newUser = new Users ('xZ4A1TuPxx-Vyywo1FanrvxGH59ZCs6X');  //new user
+  $newUser = new Users ('wUVEoYSSpzzg85pHK-dLkHMGw7tkhEmd');  //new user
   $newUser->getUser();
   $put_data = array(  //sample data
     'user' => array(
-      'first_name'=> 'Luis',
-      'last_name'=>  'Buenaventura',
-      'mobile'=>     '+639175551111',
-      'address'=>    '251 Salcedo St., Legaspi Village',
-      'city'=>       'Makati City'
+      'first_name'=> 'JC Carlo',
+      'last_name'=>  'Quintos',
+      'mobile'=>     '+639179206511',
+      'address'=>    '189 Mt Makiling St., Brgy. Palingon',
+      'city'=>       'Calamba City'
     )
   );
   $newUser->updateUser($put_data);
+    $newUser->getUser()
 
 ?>
