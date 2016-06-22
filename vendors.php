3fc852ac6e41a629fceb94ee5e9863159ca214e3
cquintos <jdquintos@up.edu.ph>
2016-06-22 13:53:49 +0800

    no purpose

--- vendors.php
+++ vendors.php
@@ -5,32 +5,26 @@
   chdir(dirname(__DIR__));
 
   require 'vendor/autoload.php';
-  require 'clientCreator.php';													
+  require 'clientCreator.php';
 
-	
-	class Vendors{
-		var $vendorToken; 		//vendor id
-		var $vendors;     		//vendor client
-									
-		//constructor							
-		function __construct($vendorToken){
-			//initialize vendor token from parameter received
-			$this->vendorToken = $vendorToken;
-		}//close constructor
 
-		//get vendor details based on vendorToken
-		function getVendor(){	
-			$response = clientCreator::getInstance()->request('GET',"vendors/$this->vendorToken");
-			$body = json_decode($response->getBody(), true);						
-			$data = json_encode($body['vendor'])."\n";
-			
-			//return json object
-			return $data;
-		}//close getVendor
-	}//close class
+  class Vendors{
+    var $vendorToken; //vendor id
+    var $vendors;     //vendor client
+
+    function __construct($vendorToken){
+      $this->vendorToken = $vendorToken;
+    }//close constructor
+
+    function getVendor(){               //retrieve a vendor obj given a vendor id
+      $tempClient = clientCreator::getInstance();
+      $response = $tempClient->request('GET',"vendors/$this->vendorToken");
+      $body = json_decode($response->getBody(), true);
+      $data = json_encode($body['vendor'])."\n";
+
+      echo $data."\n";
+    }//close getVendor
+  }//close class
 
   $newVendor = new Vendors('xZ4A1TuPxx-Vyywo1FanrvxGH59ZCs6X');
   $newVendor->getVendor();
-
-//close main php
-?>
