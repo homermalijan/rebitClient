7a147fc6ced0415bb36b57e2d6eb79106b3280ea
hoommaah <hcmalijan@up.edu.ph>
2016-06-23 15:02:21 +0800

    mergeeee

--- users.php
+++ users.php
@@ -25,19 +25,4 @@
       //echo $response->getStatusCode();
     }
   }
-
-  $newUser = new Users ('wUVEoYSSpzzg85pHK-dLkHMGw7tkhEmd');  //new user
-//  $newUser->getUser();
-  $put_data = array(  //sample data
-    'user' => array(
-      'first_name'=> 'JC Carlo',
-      'last_name'=>  'Quintos',
-      'mobile'=>     '+639179206511',
-      'address'=>    '189 Mt Makiling St., Brgy. Palingon',
-      'city'=>       'Calamba City'
-    )
-  );
-  //$newUser->updateUser($put_data);
-  $newUser->getUser();
-
 ?>
