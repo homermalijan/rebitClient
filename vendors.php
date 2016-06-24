a9399a7e612c5fc3b57d7a8361b804804e692120
hoommaah <hcmalijan@up.edu.ph>
2016-06-24 16:08:21 +0800

    oh well papel

--- vendors.php
+++ vendors.php
@@ -38,18 +38,14 @@
     //return specific user associated with this vendor
     function getUser($userId) {
       $response = clientCreator::getInstance()->request('GET',"vendors/$this->vendorToken/users/$userId");
-      $body = json_decode($response->getBody(), true);
-      $data = json_encode($body['user']);
-      return $data;
+      return $response->getBody();
     }
 
     //return specific user associated with this vendor via given email
     function getUserByEmail($userEmail) {
       $userEmail = str_replace('@', '%40', $userEmail);
       $response = clientCreator::getInstance()->request('GET',"vendors/$this->vendorToken/users/find_by_email?email=$userEmail");
-      $body = json_decode($response->getBody(), true);
-      $data = json_encode($body['user']);
-      return $data;
+      return $response->getBody();
     }
 
     //uploads new image for a user given a userId
@@ -72,17 +68,13 @@
     //get details of a credit given a creditId
     function getCredits($creditId, $get_data){
       $response = clientCreator::getInstance()->request('GET', "vendors/$this->vendorToken/credits/$creditId", ['json' => $get_data]);
-      $body = json_decode($response->getBody(), true);  //decodes the resposnce body
-      $data = json_encode($body['user']); // encodes back to json with out the user key
-      return $data;
+      return $response->getBody();
     }
 
     //get credit transaction of a given data
     function getCreditTransactions($get_data){
       $response = clientCreator::getInstance()->request('GET', "vendors/$this->vendorToken/credits", ['json' => $get_data]);
-      $body = json_decode($response->getBody(), true);  //decodes the resposnce body
-      $data = json_encode($body['user']); // encodes back to json with out the user key
-      return $data;
+      return $response->getBody();
     }
 
     //updates password of a user given the old password, new password, and new password confirmation
