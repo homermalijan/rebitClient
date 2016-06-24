5a7eef977a45fe89a96ffab3cefc2a59b8c73a2c
hoommaah <hcmalijan@up.edu.ph>
2016-06-24 16:06:31 +0800

    fixed vendor upload

--- vendors.php
+++ vendors.php
@@ -21,7 +21,7 @@
 			$body = json_decode($response->getBody(), true);
 			$data = json_encode($body['vendor']);
 			//return json object
-			echo $data;
+			return $data;
 		}//close getVendor
 
     //update vendor attributes with given params
@@ -32,9 +32,7 @@
     //return all users associated with this vendor
     function getUsers() {
       $response = clientCreator::getInstance()->request('GET',"vendors/$this->vendorToken/users");
-      $body = json_decode($response->getBody(), true);  //decodes the resposnce body
-      $data = json_encode($body['user']); // encodes back to json with out the user key
-      return $data;
+      return $response->getBody();
     }
 
     //return specific user associated with this vendor
@@ -54,12 +52,17 @@
       return $data;
     }
 
-    //--------ISSUES HERE--------
     //uploads new image for a user given a userId
     function uploadPhoto($userId, $encodedImage){
-      $response = clientCreator::getInstance()->request('POST',"vendors/$this->vendorToken/users/$userId/data=>image/jpg;base64".$encodedImage);
+      $response = clientCreator::getInstance()->request('POST',"vendors/$this->vendorToken/users/$userId/uploads/add_photo_id", ['file' => $encodedImage]);
+      echo $response->getStatusCode();
     }
-    //---------------------------
+
+    //uploads new image for a user given a userId
+    function uploadProofOfResidence($userId, $encodedImage){
+      $response = clientCreator::getInstance()->request('POST',"vendors/$this->vendorToken/users/$userId/uploads/add_proof_of_residence", ['file' => $encodedImage]);
+      echo $response->getStatusCode();
+    }
 
     //update user with given userId with given put_data
     function updateUser($userId, $put_data){
@@ -82,19 +85,17 @@
       return $data;
     }
 
-    //--------ISSUES HERE --------
     //updates password of a user given the old password, new password, and new password confirmation
     //new password and new password confirmation must match
-    function updateUserPassword($userId, $old_password, $password, $password_confirmation){
-      $response = clientCreator::getInstance()->request('PUT',"vendors/$this->vendorToken/users/$userId/update_password", ['old_password' => $old_password], ['password' => $password], ['password_confirmation' => $password_confirmation]);
+    function updateUserPassword($userId, $put_data){
+      $response = clientCreator::getInstance()->request('PUT',"vendors/$this->vendorToken/users/$userId/update_password", $put_data);
     }
-    //---------------------------
 
     //add user to this vendor
     function addUser($post_data){
       $response = clientCreator::getInstance()->request('POST',"vendors/$this->vendorToken/users", ['json' => $post_data]);
     }
-	}//close class
+	}//close Vendor class
 
 //close main php
 ?>
