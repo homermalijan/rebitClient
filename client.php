0efad9606d0bff5c57982879cd2a916a78eeac52
hoommaah <hcmalijan@up.edu.ph>
2016-06-27 12:28:18 +0800

    finished vendor remittance

--- client.php
+++ client.php
@@ -35,16 +35,16 @@
       )
     )
   );
-  $newRemittance->saveRemittance($newVendor->vendorToken, 10876, $data);
+  // $newRemittance->saveRemittance($newVendor->vendorToken, 10876, $data);
   // $newVendor->getVendor();
   // $newVendor->getCreditTransactions();
   //$newVendor->showOutgoingRemittances(12672);
-  $newVendor->getUsers();
+  // $newVendor->getUsers();
   // $newVendor->updateUser(12672, $dataRemit);
   // $newVendor->deleteUser(12677);
   // echo $newVendor->getUser(12677);
-  $nr = new VendorRecipients(1);
-  echo $nr->getRecipient($newVendor->vendorToken, 10876);
+  // $nr = new VendorRecipients(1);
+  // echo $nr->getRecipient($newVendor->vendorToken, 10876);
   // echo $newVendor->getVendor();
   // $newUser = new Users ('wUVEoYSSpzzg85pHK-dLkHMGw7tkhEmd');    //new user
   // echo $newUser->getUser();
@@ -53,53 +53,12 @@
   //header('Content-Length: ' . filesize($file));
   // echo file_get_contents($file);
 
-  // $newVendor->uploadProofOfResidence(12677, base64_encode('/home/hoommaah/Desktop/rebitClient/upload.jpg'));
-  // $post_data = array (
-  //   'user'=> array (
-  //     'id'=> 1,
-  //     'first_name'=> 'Juan',
-  //     'last_name'=> 'dela Cruz',
-  //     'email'=> 'juan@example.com',
-  //     'birthday'=> null,
-  //     'mobile'=> null,
-  //     'wallet_address'=> null,
-  //     'institutional'=> false,
-  //     'address'=> null,
-  //     'city'=> null,
-  //     'country'=> null,
-  //     'postal_code'=> null,
-  //     'maximum_sends_per_day'=> 10,
-  //     'mobile_confirmed'=> true,
-  //     'kyc_level'=> 0,
-  //     'identification'=> array (
-  //       'url'=> null,
-  //       'thumb'=> array (
-  //         'url'=> null
-  //       )
-  //     ),
-  //     'identification_confirmed'=> false,
-  //     'proof_of_address'=> array (
-  //       'url'=> null,
-  //       'thumb'=> array(
-  //         'url'=> null
-  //       )
-  //     ),
-  //     'proof_of_address_confirmed'=> false,
-  //     'recipient_ids'=> [
-  //       1,
-  //       2,
-  //       3
-  //     ],
-  //     'remittance_ids'=> [
-  //       1,
-  //       2
-  //     ],
-  //     'total_remittances_today'=> 1000,
-  //     'vendor_uploaded_identification'=> null,
-  //     'vendor_uploaded_proof_of_address'=> null
-  //   ),
-  //   'password'=> 'yourpassword',
-  //   'api_token'=> 'AAAAAAAAAA-t_-DCsC-sssss-xxxxx'
-  // );
-
+  $post_data = array (
+      'amount'=> "1000.00",
+      'currency'=> "PHP",
+      'strategy'=> "pickup",
+      'provider'=> "ABC",
+      'province'=> "Abra"
+  );
+  $newRemittance->calculateRemittance($newVendor->vendorToken, 10876, $post_data);
 ?>
